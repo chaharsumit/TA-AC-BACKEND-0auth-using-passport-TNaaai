ce96f0690da781a5b43d8137fa649ae999fb31e7
Sumit Chahar <chahar.sumit888@gmail.com>
2021-12-22 23:07:56 +0530

    Add github and google login authentication, add checks for user having same google & github email

---
+++ block-BNaaee/Login-App/models/User.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  name: String,
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  photo: String
+}, { timestamps: true });
+
+
+let User = mongoose.model('User', userSchema);
+
+module.exports = User;
